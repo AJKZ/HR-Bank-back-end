0f890eab8d25f0f2e6f96445c4f3e5c6af3152cc
Alan Zhong <0977837@hr.nl>
2019-04-24 16:01:58 +0200

    add functionality for checking balance, add simple front-end connection

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//import * as corsMiddleware from "restify-cors-middleware"
+const corsMiddleware = require('restify-cors-middleware');
 const restify = require('restify');
 const mongoose = require('mongoose');
 
@@ -6,6 +8,14 @@ const server = restify.createServer({
     version: '1.1.0'
 });
 
+const cors = corsMiddleware({
+    origins: ["*"],
+    allowHeaders: ['Authorization'],
+    exposeHeaders: ['Authorization']
+});
+
+server.pre(cors.preflight);
+server.use(cors.actual);
 server.use(restify.plugins.bodyParser());
 
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
@@ -16,7 +26,7 @@ const User = mongoose.model('User', {
     balance: Number
 });
 
-server.post('/login', async (req, res, next) => {
+server.post('/login', (req, res, next) => {
     const iban = req.body.iban
     const pin = req.body.pin
 
@@ -30,18 +40,36 @@ server.post('/login', async (req, res, next) => {
             }
 
             res.send(200)
-        })
+        });
 
     console.log(iban, pin)
     res.send(200, 'success')
 });
 
-server.post('/withdraw', async (req, res, next) => {
+server.post('/getbalance', (req, res, next) => {
+    const iban = req.body.iban
+    const pin = req.body.pin
+
+    return User.findOne({$and: [{iban: iban}, {pin: pin}]})
+    .then((result) => {
+        res.send(200, result.balance);
+    })
+    .catch(function handleWithdrawError(error) {
+        res.send(500, error)
+    });
+});
+
+server.post('/withdraw', (req, res, next) => {
     const iban = req.body.iban
     const amount = req.body.amount
 
-    const result = await User.findOneAndUpdate({ iban: iban }, { $inc: { balance: -amount } }, { new: true });
-    res.send(200, result);
+    User.findOneAndUpdate({ iban: iban }, { $inc: { balance: -amount } }, { new: true })
+        .then(function handleWithdrawResult(result) {
+            res.send(200, result);
+        })
+        .catch(function handleWithdrawError(error) {
+            res.send(500, error)
+        });
 });
 
 /*
